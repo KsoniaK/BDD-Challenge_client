a070b1e805928961b4c13041820e02e795a7629f
sonia kechit <kechits@yahoo.com>
2025-11-16 00:43:41 +0100

    API test

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-const API = process.env.REACT_APP_API_URL;
+// const API = process.env.REACT_APP_API_URL;
+const API = "https://bdd-challenge-server.onrender.com";
 console.log("API URL utilisée :", API);
 
 
